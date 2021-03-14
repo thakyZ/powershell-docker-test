93009e3229f67f161531928fb058b332b9bf26cc
Neko Boi Nick <950594+thakyZ@users.noreply.github.com>
2021-03-14 15:47:56 -0700

    Update download_file.ps1

--- download_file.ps1
+++ download_file.ps1
@@ -9,10 +9,12 @@ $BYOND_MAJOR=513
 $BYOND_MINOR=1536
 
 try {
-	Invoke-WebRequest -Uri "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond.zip" -UseBasicParsing -OutFile byond.zip
+    Invoke-WebRequest -Uri "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond.zip" -UseBasicParsing -OutFile byond.zip
     Expand-Archive byond.zip -DestinationPath .
     Copy-Item -Path byond\* -Recurse -Destination .
     Remove-Item -Path byond.zip
+    Get-ChildItem -Path .
+    Get-ChildItem -Path bin
 }
 catch {
 	Write-Error "Failed."
